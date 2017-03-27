26a1565e8db524284c230ed14742eeee7ac85866
Artem Zakirullin <artem.zakirullin@gmail.com>
2017-03-27 22:46:15 +0500

    add EntityFilter

--- src/Pipedrive.php
+++ src/Pipedrive.php
@@ -3,11 +3,11 @@
 namespace Zakirullin\Pipedrive;
 
 /**
- * @property Entity organizations
- * @property Entity activities
- * @property Entity deals
- * @property Entity persons
- * @property Entity notes
+ * @property EntityFilter organizations
+ * @property EntityFilter activities
+ * @property EntityFilter deals
+ * @property EntityFilter persons
+ * @property EntityFilter notes
  */
 class Pipedrive
 {
@@ -22,26 +22,31 @@ class Pipedrive
     protected $apiUrl;
 
     /**
-     * @var array $shortFields
+     * @var array $fields
      */
-    protected $shortFields;
+    protected $fields;
 
-    public function __construct($apiToken, $shortFields = [], $apiUrl = 'https://api.pipedrive.com/v1')
+    /**
+     * @var array $ids
+     */
+    protected $idFields = [
+        'organizations' => 'org_id',
+    ];
+
+    const WALK_STEP = 500;
+
+    // TODO add version and scheme
+    public function __construct($apiToken, $fields = [], $apiUrl = 'https://api.pipedrive.com/v1')
     {
         $this->apiToken = $apiToken;
-        $this->shortFields = $shortFields;
+        $this->shortFields = $fields;
         $this->apiUrl = $apiUrl;
 
         return $this;
     }
 
-    public function process($entity, $method, $data = [], $params = [])
-    {
-        return $this->processResponse($this->sendRequest($entity, $method, $data, $params));
-    }
-
     // TODO Checking for parents
-    public function sendRequest($entity, $method, $data = [], $params = [])
+    public function sendRequest(EntityFilter $entity, $method, $data = [], $params = [])
     {
         $url = $this->buildApiUrl($entity, $params, $method);
 
@@ -58,10 +63,40 @@ class Pipedrive
         }
     }
 
+    public function walkAll(EntityFilter $entity, $callback)
+    {
+        $start = 0;
+        do {
+            $terminate = true;
+
+            $params = ['limit' => static::WALK_STEP, 'start' => $start];
+            $response = $this->sendRequest($entity, 'get', [], $params);
+            $isPaginationExists = isset($response->additional_data->pagination);
+            $isMoreItems = $isPaginationExists && $response->additional_data->pagination->more_items_in_collection;
+            if ($response->success == 'true') {
+                if ($isMoreItems) {
+                    $start = $response->additional_data->pagination->next_start;
+                }
+
+                if (is_array($response->data)) {
+                    foreach ($response->data as $data) {
+                        $terminate = $callback($data);
+                        if ($terminate) {
+                            break;
+                        }
+                    }
+                } else {
+                    $terminate = $callback($response->data);
+                }
+            }
+
+        } while (!$terminate && $isMoreItems);
+    }
+
     // TODO excetpion
     public function __get($entityType)
     {
-        return new Entity($this, $entityType);
+        return new EntityFilter($this, $entityType);
     }
 
     public function getApiToken()
@@ -96,46 +131,65 @@ class Pipedrive
         return $this;
     }
 
-    public function getShortFields()
+    public function getFields()
     {
-        return $this->shortFields;
+        return $this->fields;
     }
 
     /**
-     * @param Entity $entity
-     * @param string $field
-     * @return null|string
-     */
-    public function getShortField($entity, $field)
-    {
-        return isset($this->shortFields[$entity->getType()][$field]) ? $this->shortFields[$entity->getType()][$field] : null;
-    }
-
-    /**
-     * @param array $shortFields
+     * @param array $fields
      * @return $this
      */
-    public function setShortFields($shortFields)
+    public function setFields($fields)
     {
-        $this->shortFields = $shortFields;
+        $this->setFields = $fields;
+    }
+
+    public function getIdFields()
+    {
+        return $this->idFields;
+    }
+
+    public function setIdFields($idFields)
+    {
+        $this->$idFields = $idFields;
+
+        return $this;
     }
 
     /**
-     * @param Entity $entity
+     * @param EntityFilter $entity
      * @param array $params
      * @param null|string $method
      * @return string
      */
-    protected function buildApiUrl($entity, $params = [], $method = null)
+    protected function buildApiUrl(EntityFilter $entityFilter, $params = [], $method = null)
     {
         $url = $this->getApiUrl();
 
-        if (($parent = $entity->getParent()) && $method == 'get') {
-            $url .= "/{$parent->getType()}/{$parent->getId()}/{$entity->getType()}";
+        $condition = $entityFilter->getCondition();
+        if (is_array($condition)) {
+            if ($condition) {
+                // BuildSearchQuery
+                $url .= "/searchResults/field";
+                $params['term'] = array_values($condition)[0];
+                $params['field_type'] = $entityFilter->buildSearchField();
+                $params['field_key'] = $this->getField($entityFilter, array_keys($condition)[0]);
+                $params['return_item_ids'] = 1;
+                $params['exact_match'] = (int)$entityFilter->isExactMatch();
+            } else {
+                throw new \Exception('Condition is empty!');
+            }
         } else {
-            $url .= "/{$entity->getType()}";
-            if ($entity->getId() !== null) {
-                $url .= "/{$entity->getId()}";
+            if (($prev = $entityFilter->getPrev()) && $prev->getId() && $method == 'get') {
+                // BuilRelationQuery
+                $url .= "/{$prev->getType()}/{$prev->getId()}/{$entityFilter->getType()}";
+            } else {
+                // BuildQuery
+                $url .= "/{$entityFilter->getType()}";
+                if ($entityFilter->getId() !== null) {
+                    $url .= "/{$entityFilter->getId()}";
+                }
             }
         }
 
@@ -145,4 +199,3 @@ class Pipedrive
         return $url;
     }
 }
-
